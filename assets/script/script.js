0572c85417ee9c9fd4dbab77e4a38b912be2a973
mlcundayag <mlcundayag@gmail.com>
2022-09-04 20:35:57 -1000

    trial adding parameters to ending quiz

--- assets/script/script.js
+++ assets/script/script.js
@@ -6,6 +6,7 @@ const answerButtonsElement = document.getElementById("answer-buttons");
 const timerElement = document.getElementById("timer");
 const timerContainerElement = document.getElementById("timer-container");
 const instructionElement = document.getElementById("instruction");
+const statusElement = document.getElementById("status");
 
 let shuffledQuestions, currentQuestionIndex
 let timerCount = 60;
@@ -24,31 +25,33 @@ function startGame() {
     questionContainerElement.classList.remove("hide");
     timerContainerElement.classList.remove("hide");
     instructionElement.classList.add("hide");
-    setNextQuestion();
     startTimer()
+    if (timerCount < 60) {
+        timerElement.innerText = 60
+        }
+    setNextQuestion();
 }
 
 //Timer
 function startTimer() {
     //Sets timer
-    timer = setInterval(function() {
+    const timer = setInterval(() => {
         timerCount--;
         timerElement.textContent = timerCount;
-        if (timerCount >= 0 ) {
-            if (isWin && timerCount > 0)
-            clearInterval(timer);
-            winGame();
-        }
-        if (timerCount === 0) {
-            clearInterval(timer);
-            loseGame();
-        }
-    }, 1000);
+        if (timerCount == 0 ) {
+        clearInterval(timer);
+        endGame();
+        } 
+    }, 100);
 };
 
 function setNextQuestion() {
+    if (timerCount == 0) {
+    endGame();
+    } else {
     resetState()
     showQuestion(shuffledQuestions[currentQuestionIndex])
+    }
 }
 
 function showQuestion(question) {
@@ -83,8 +86,9 @@ function selectAnswer(e) {
     if (shuffledQuestions.length > currentQuestionIndex + 1){
         nextButton.classList.remove("hide")
     } else {
-        startButton.innerText = "Restart"
+        startButton.innerText = "Try Again"
         startButton.classList.remove("hide")
+        clearInterval(timer);
     }
 }
 
@@ -100,6 +104,10 @@ function setStatusClass(element, correct) {
 function clearStatusClass(element) {
     element.classList.remove("correct")
     element.classList.remove("wrong")
+}
+
+function endGame() {
+    timerElement.textContent = 0;
 }
 
 const questions = [
@@ -149,99 +157,5 @@ const questions = [
             ]
         },
     ];
-
-// //declaring variables
-
-// var timerElement = document.getElementById("timer");
-// var questionElement = document.getElementById("question");
-// var multipleChoice = document.getElementById("container");
-// var choiceOne = document.getElementById("choice1");
-// var choiceTwo = document.getElementById("choice2");
-// var choiceThree = document.getElementById("choice3");
-// var choiceFour = document.getElementById("choice4")
-// var scoreElement = document.getElementById("score");
-// var initialsElement = document.getElementById("initials");
-// var highScoreElement = document.getElementById("highscores");
-// var startButton = document.getElementById("start-button");
-// var resetButton = document.getElementById("reset-button");
-// var againButton = document.getElementById("try-again");
-// var submitButton = document.getElementById("submit");
-// var answerButton = document.getElementById("answer");
-// var nextButton = document.getElementById("next")[0];
-// // var randomQuestions = randomQs();
-
-// var timerCount
-// var myCorrect = 0;
-// var showQuestions = " ";
-// var userChoice = "";
-// var start = true;
-
-// // let finalScore;
-
-// //Question Variables
-// var quiz = [
-//     {   
-//         questionNumber: 0,
-//         question: "What is a programming language that was invented by Brendan Eich, which can add interactivity to a website?",
-//         choices: [
-//             { answer: "HTML", correct: false },
-//             { answer: "CSS", correct: false },
-//             { answer: "JavaScript", correct: true },
-//             { answer: "MS Excel", correct: false }
-//         ]
-//     },
-//     {
-//         questionNumber: 1,
-//         question: "Is JavaScript a case-sensitve language?",
-//         choices: [
-//             { answer: "Yes, the language keywords,variables, functions names, and any other identifiers must always be typed with a consistent 'lower case' of letters", correct: false },
-//             { answer: "Yes, Javascript must always use 'upper case' letters", correct: false },
-//             { answer: "Yes, the language keywords,variables, functions names, and any other identifiers must always be typed with a combination of 'numbers & symbols' and letters", correct: false },
-//             { answer: "Yes, it must have a consistent 'capitalization' of letters", correct: true }
-//         ]
-//     },
-//     {
-//         questionNumber: 2,
-//         question: "What method will you use to convert a JavaScript value to a JSON string",
-//         choices: [
-//             { answer: "JSON.parse()", correct: false },
-//             { answer: "JSON.stringify()", correct: true },
-//             { answer: "JSON.dumps()", correct: false },
-//             { answer: "JSON.loads()", correct: false }
-//         ]
-//     },
-//     {
-//         questionNumber: 3,
-//         question: "In how many ways a JavaScript can be involved in an HTML file?",
-//         choices: [
-//             { answer: "1", correct: false },
-//             { answer: "2", correct: false },
-//             { answer: "3", correct: true },
-//             { answer: "0", correct: false }
-//         ]
-//     },
-//     {
-//         questionNumber: 4,
-//         question: "What is NaN in Javascript?",
-//         choices: [
-//             { answer: "Not a Number", correct: true },
-//             { answer: "Not a Null", correct: false },
-//             { answer: "Not a Nomenclature", correct: false },
-//             { answer: "Non artis Natura", correct: false }
-//         ]
-//     },
-// ];
-
-// //Start Quiz with a button
-// function startQuiz() {
-//     timerCount = 60
-//     startButton.disabled = true;
-//     resetButton.disabled = true;
-//     againButton.disabled = true;
-//     //nextQuestion();
-//     startTimer()
-// }
-
-// 
 
 
